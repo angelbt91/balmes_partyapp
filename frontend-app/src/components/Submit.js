ec9db1da6d769587134858896e2090a13d82aae4
Angel <angelbt91@gmail.com>
2019-11-19 22:01:41 +0100

    Funcionalidad de subir imagenes creada!

--- frontend-app/src/components/Submit.js
+++ frontend-app/src/components/Submit.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './bootstrap.min.css';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import SendIcon from '@material-ui/icons/Send';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import { ToastContainer, toast } from 'react-toastify';
+import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
@@ -15,18 +15,15 @@ function Submit() {
     const [message, setMessage] = useState("");
     const [image, setImage] = useState("");
     const [disabled, setDisabled] = useState(false);
-
     const data = {
         name: name,
         message: message,
         image: image
     };
 
-    // TODO: deshabilitar el botón hasta que se haya efectuado el envio o fallado
-
-    // método para hacer el post
-    const fetchdata = async () => {
-
+    // metodo para leer la imagen
+    const uploadImage = async () => {
+        // si no hay ni mensaje ni imagen, devolvemos error
         if (data['message'] === "" && data['image'] === "") {
             toast.warn('Please write a message, attach a photo... Or both!', {
                 position: "top-center",
@@ -37,67 +34,125 @@ function Submit() {
                 draggable: true
             });
             setDisabled(false);
-        } else {
+            return;
+        }
 
-            const url = 'http://127.0.0.1/api/postmessage';
+        // si no hay imagen, saltamos directamente a enviar mensaje
+        if (data.image === "") {
+            uploadMessage();
+            return;
+        }
 
-            const options = {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: new Headers({
-                    Accept: 'application/json',
-                    'Content-type': 'application/json',
-                    'Access-Control-Allow-Headers': 'Content-Type',
-                }),
-                mode: 'cors'
-            };
+        // si todo bien, cargamos el filereader
+        let reader = new FileReader();
 
-            return fetch(url, options)
-                .then(response => {
-                    if (response.status === 200) {
-                        console.log("Éxito. Status: " + response.status + ". Texto: " + response.statusText + ". Body:\n" + options['body']);
-                        toast.success('Message succesfully sent!', {
-                            position: "top-center",
-                            autoClose: 5000,
-                            hideProgressBar: true,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true
-                        });
-                        setDisabled(false);
+        reader.onload = () => {
+            data["image"] = reader.result;
+            console.log(data.image);
+            publishImage();
+        }
 
-                        // vaciamos el nombre
-                        const nameField = document.getElementById("nameField");
-                        nameField.value = "";
-                        setName("");
+        reader.onerror = () => {
+            toast.error('Error reading the image. Please retry.', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true
+            });
+        }
 
-                        // vaciamos el mensaje
-                        const messageField = document.getElementById("messageField");
-                        messageField.value = "";
-                        setMessage("");
+        reader.readAsDataURL(data["image"]);
 
-                        // vaciamos la imagen
-                        const imageField = document.getElementById("imageField");
-                        imageField.value = "";
-                        setImage("");
+    };
 
-                        // TODO: Borrar los campos
-                    } else {
-                        console.log("Error " + response.status + ".\n\n" + response.body);
-                        toast.error('Message could not be sent. Please retry.', {
-                            position: "top-center",
-                            autoClose: 5000,
-                            hideProgressBar: true,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true
-                        });
-                        setDisabled(false);
-                        return Promise.reject(response.status);
-                    }
-                }).catch(error => {
-                    console.log("Error: " + error);
-                    toast.error('Message could not be sent. Please retry.', {
+    // método para hacer el post de imagen
+    const publishImage = async () => {
+
+        // bases para hacer el fetch
+        const url = 'https://api.cloudinary.com/v1_1/themrangel/auto/upload';
+        const imageData = {
+            "file": data.image,
+            "upload_preset": "iqxfcs3m"
+        };
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(imageData),
+            headers: new Headers({
+                Accept: 'application/json',
+                'Content-type': 'application/json',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            }),
+            mode: 'cors'
+        };
+
+        // avisamos de que vamos a subir la imagen
+        toast.info('Uploading image...', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true
+        });
+
+        fetch(url, options)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    setDisabled(false);
+                    return Promise.reject(response.status);
+                }
+            })
+            .then(response => {
+                toast.success('Image uploaded!', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true
+                });
+                data.image = response["eager"][0]["url"];
+                uploadMessage();
+            })
+            .catch(error => {
+                toast.error('Image could not be sent. Please retry.', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true
+                });
+                setDisabled(false);
+                console.log("Error al publicar imagen. Error número " + error.status + ". Contenido de la request: \n" + options.body);
+                return Promise.reject(error.status);
+            });
+    };
+
+
+// subir mensaje
+    const uploadMessage = async () => {
+
+        // subida del mensaje
+        const url = 'http://127.0.0.1/api/postmessage';
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: new Headers({
+                Accept: 'application/json',
+                'Content-type': 'application/json',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            }),
+            mode: 'cors'
+        };
+        return fetch(url, options)
+            .then(response => {
+                if (response.status === 200) {
+                    toast.success('Message succesfully sent!', {
                         position: "top-center",
                         autoClose: 5000,
                         hideProgressBar: true,
@@ -106,9 +161,37 @@ function Submit() {
                         draggable: true
                     });
                     setDisabled(false);
-                });
 
-        }
+                    // vaciamos el nombre
+                    const nameField = document.getElementById("nameField");
+                    nameField.value = "";
+                    setName("");
+
+                    // vaciamos el mensaje
+                    const messageField = document.getElementById("messageField");
+                    messageField.value = "";
+                    setMessage("");
+
+                    // vaciamos la imagen
+                    const imageField = document.getElementById("imageField");
+                    imageField.value = "";
+                    setImage("");
+
+                } else {
+                    return Promise.reject(response.status);
+                }
+            }).catch(error => {
+                console.log("Error al subir el mensaje. Error número " + error.status + ".\n\n" + error.body);
+                toast.error('Message could not be sent. Please retry.', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true
+                });
+                setDisabled(false);
+            });
     };
 
 
@@ -166,7 +249,7 @@ function Submit() {
                         disabled={disabled}
                         onClick={() => {
                             setDisabled(true);
-                            fetchdata();
+                            uploadImage();
                         }}>
                         Send to the TV
                     </Button>
@@ -176,7 +259,7 @@ function Submit() {
                     type="file"
                     accept="image/*;capture=camera"
                     onChange={(e) => {
-                        setImage(e.target.value)
+                        setImage(e.target.files[0]);
                     }}
                 />
             </div>
